eb1d5e497b6c58f9b05e128731675c8712e6c3c2
Arthur Axiotes <axiotesarthur@gmail.com>
2024-11-18 10:46:25 -0300

    feat: criacao do componente de card product

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardProductComponent } from './card-product/card-product.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CardProductComponent
+  ],
   imports: [
     CommonModule
   ]
