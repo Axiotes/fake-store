22471ac18aca0eb234e3b6d58fb04eb00427c649
Arthur Axiotes <axiotesarthur@gmail.com>
2024-11-18 11:51:37 -0300

    feat: estrutura html e estilizacao do card

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardProductComponent } from './card-product/card-product.component';
 
-
-
 @NgModule({
   declarations: [
     CardProductComponent
