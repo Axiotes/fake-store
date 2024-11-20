d1512cc4ef09184950818c02b8fb42bc6cb9d308
Arthur Axiotes <axiotesarthur@gmail.com>
2024-11-20 09:50:07 -0300

    feat: criacao de servico para pegar todas as categorias da api

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiCategoriesService } from '../../services/api-categories.service';
+import { Category } from '../../../types/category.type';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public allCategories!: Category[];
 
+  constructor(private apiCategoriesService: ApiCategoriesService) {}
+
+  ngOnInit(): void {
+    this.apiCategoriesService.getCategories().subscribe({
+      next: (response) => {
+        this.allCategories = response;
+        console.log(this.allCategories);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
---
+++ src/app/services/api-categories.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../../types/category.type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiCategoriesService {
+  private baseURL: string = 'https://fakestoreapi.com';
+
+  constructor(private http: HttpClient) {}
+
+  public getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${this.baseURL}/products/categories`);
+  }
+}
