549fbc3b8a9527bd43c25fc2f6c8fd1ca6c85545
Arthur Axiotes <axiotesarthur@gmail.com>
2024-11-22 00:09:52 -0300

    wip

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../../services/storage.service';
+import { Product } from '../../../types/product.type';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrl: './cart.component.scss',
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  public products: Product[] = [];
 
+  constructor(private storageService: StorageService) {}
+
+  ngOnInit(): void {
+    const value = this.storageService.getItem('favorites');
+
+    if (value) {
+      this.products = value.products;
+    }
+  }
 }
