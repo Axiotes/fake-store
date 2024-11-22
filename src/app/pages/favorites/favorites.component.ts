2875bb4ba43a27179c9d02dd874fb89ff0296745
Arthur Axiotes <axiotesarthur@gmail.com>
2024-11-21 21:33:27 -0300

    feat: acessar localstorage e exibir produto ao iniciar a pagina

--- src/app/pages/favorites/favorites.component.ts
+++ src/app/pages/favorites/favorites.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../../services/storage.service';
+import { Product } from '../../../types/product.type';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
-  styleUrl: './favorites.component.scss'
+  styleUrl: './favorites.component.scss',
 })
-export class FavoritesComponent {
+export class FavoritesComponent implements OnInit {
+  public products: Product[] = [];
 
+  constructor(private storageService: StorageService, private router: Router) {}
+
+  ngOnInit(): void {
+    const value = this.storageService.getItem('favorites');
+
+    if (value) {
+      this.products = value.products;
+    }
+  }
+
+  public selectedProduct(id: number): void {
+    this.router.navigate(['/details', id]);
+  }
 }
