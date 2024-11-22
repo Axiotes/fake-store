6168b4c5834253a904e6c71ae7c37f3c006b7f25
Arthur Axiotes <axiotesarthur@gmail.com>
2024-11-21 22:11:48 -0300

    feat: criacao de componente e rota do carrinho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,20 @@ import { HomeComponent } from './pages/home/home.component';
 import { CategoryComponent } from './pages/category/category.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'category/:category', component: CategoryComponent },
   { path: 'details/:id', component: DetailsComponent },
   { path: 'favorites', component: FavoritesComponent },
+  { path: 'cart', component: CartComponent },
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: '**', redirectTo: '/home' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
