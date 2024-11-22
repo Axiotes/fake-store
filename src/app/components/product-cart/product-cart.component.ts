f39b17bc74c6ab9d059bc431a3f628eb72259597
Arthur Axiotes <axiotesarthur@gmail.com>
2024-11-21 23:00:35 -0300

    feat: componente do produto dinamico

--- src/app/components/product-cart/product-cart.component.ts
+++ src/app/components/product-cart/product-cart.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Trash2 } from 'lucide-angular';
 import { LucideIconData } from 'lucide-angular/icons/types';
 
@@ -8,5 +8,31 @@ import { LucideIconData } from 'lucide-angular/icons/types';
   styleUrl: './product-cart.component.scss',
 })
 export class ProductCartComponent {
+  @Output()
+  public remove: EventEmitter<void> = new EventEmitter<void>();
+
+  @Output()
+  public alterQuantity: EventEmitter<string> = new EventEmitter<string>();
+
+  @Input()
+  public image!: string;
+
+  @Input()
+  public title!: string;
+
+  @Input()
+  public price!: number;
+
+  @Input()
+  public quantity!: number;
+
   public trash: LucideIconData = Trash2;
+
+  public removeProduct(): void {
+    this.remove.emit();
+  }
+
+  public alterQuantityProduct(value: string): void {
+    this.alterQuantity.emit(value);
+  }
 }
