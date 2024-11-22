d5da32dd679985731956f99a8dd5dbad25b6b9f0
Arthur Axiotes <axiotesarthur@gmail.com>
2024-11-21 21:04:48 -0300

    feat: criacao de componente e definicao de rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CategoryComponent } from './pages/category/category.component';
 import { DetailsComponent } from './pages/details/details.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'category/:category', component: CategoryComponent },
   { path: 'details/:id', component: DetailsComponent },
+  { path: 'favorites', component: FavoritesComponent },
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: '**', redirectTo: '/home' },
 ];
