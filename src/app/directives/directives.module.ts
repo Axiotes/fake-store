3bc56fe9557b84d6873fec8abc6497dbf35b29e2
Arthur Axiotes <axiotesarthur@gmail.com>
2024-11-23 13:57:51 -0300

    feat: criacao de diretiva para alterar cor ao passar o mouse

---
+++ src/app/directives/directives.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeColorDirective } from './change-color.directive';
+
+@NgModule({
+  declarations: [
+    ChangeColorDirective
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ChangeColorDirective
+  ]
+})
+export class DirectivesModule { }
